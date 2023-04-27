09a7b958febbd79eb27f3fa4900205de604cb106
Ashish-Chauhan <chauhankrashish@gmail.com>
2023-04-27 11:48:20 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,33 @@
+var config = {
+    texts: [
+        // "give it to me",
+        // "Beloved little cutie",
+        // "Today is your birthday",
+        // "This is what we are together",
+        // "It's my second birthday",
+        // "Last year's birthday",
+        // "I still remember eating skewers at Wangpo",
+        // "I want to eat well this year",
+        // "I want to feed my cute pigs to the full",
+        // "and take away",
+        
+        'जीवन के रास्ते हमेशा गुलजार रहें',
+        'चेहरे पर आपके सदा ही मुस्कान रहे',
+        'देता है दिल यह दुआ आपको',
+        'ज़िन्दगी में हर दिन खुशियों की बहार रहे!',
+        'Little Sister'
+    ],
+    imgs: {
+        "Little Sister": "./imgs/xiaokeai.jpg",
+    },
+    desc: {
+        turn_on: "click me",
+        play: "music~",
+        banner_coming: "Love",
+        balloons_flying: "Looks like something is missing",
+        cake_fadein: "Cake?",
+        light_candle: "candle?",
+        wish_message: "Happy Birthday Shuhani",
+        story: "A MESSAGE FOR YOU",
+    }
+};
